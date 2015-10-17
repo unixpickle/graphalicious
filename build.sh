09dd56e9cc297976c6a5a03cca61465efd4f3b43
Alex Nichol <unixpickle@gmail.com>
2015-10-17 12:05:16 -0400

    renamed y_label_content_view to ylcv

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 jsbuild -license=LICENSE -name="graphalicious.base" -version=`cat VERSION` -output build_base.js -includeAPI src/base/*.js
-jsbuild -license=LICENSE -name="graphalicious.ylcv" -version=`cat VERSION` -output build_ylcv.js -includeAPI src/y_label_content_view/*.js
+jsbuild -license=LICENSE -name="graphalicious.ylcv" -version=`cat VERSION` -output build_ylcv.js -includeAPI src/ylcv/*.js
 cat build_base.js build_ylcv.js >build.js
 rm build_base.js build_ylcv.js
