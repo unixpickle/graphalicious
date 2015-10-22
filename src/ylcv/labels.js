94cf1cf4dae76b62b747f89dca6809a5798c0561
Alex Nichol <unixpickle@gmail.com>
2015-10-21 20:53:48 -0400

    created LabelSettings structure

--- src/ylcv/labels.js
+++ src/ylcv/labels.js
@@ -1,14 +1,33 @@
-function Labels(text, values, font) {
+function LabelSettings(attrs) {
+  this.leftMargin = attrs.leftMargin || LabelSettings.DEFAULT_MARGIN;
+  this.rightMargin = attrs.rightMargin || LabelSettings.DEFAULT_MARGIN;
+  this.textColor = attrs.textColor || LabelSettings.DEFAULT_TEXT_COLOR;
+  this.font = attrs.font || LabelSettings.DEFAULT_FONT;
+  this.opacity = attrs.opacity || 1;
+}
+
+LabelSettings.DEFAULT_MARGIN = 10;
+LabelSettings.DEFAULT_TEXT_COLOR = '#999';
+LabelSettings.DEFAULT_FONT = '10px sans-serif';
+
+LabelSettings.prototype.margin = function() {
+  return this.leftMargin + this.rightMargin;
+};
+
+function Labels(text, values, settings) {
   if (!Array.isArray(text) || !Array.isArray(values) || text.length !== values.length ||
       this.text.length < 2) {
     throw new Error('invalid arguments');
   }
   this.text = text;
   this.values = values;
-  this.width = 0;
+  this.settings = settings;
+
+  this._width = 0;
   for (var i = 0, len = text.length; i < len; ++i) {
-    this.width = Math.max(this.width, measureLabel(text[i], font));
+    this._width = Math.max(this._width, measureLabel(text[i], this.settings.font));
   }
+  this._width += this.settings.margin();
 }
 
 Labels.widthContext = document.createElement('canvas').getContext('2d');
@@ -18,13 +37,18 @@ Labels.measureLabel = function(text, font) {
   return Labels.widthContext.measureText(text);
 };
 
-Labels.prototype.draw = function(ctx, leftX, topY, height) {
+Labels.prototype.width = function() {
+  return this.width;
+};
+
+Labels.prototype.draw = function(ctx, leftX, topY, bottomY) {
   ctx.textBaseline = 'middle';
   ctx.textAlign = 'end';
 
-  var spacing = (height / (this.text.length - 1));
-  for (var i = 0, len = this.text.length; i < len; ++i) {
-    var y = topY + spacing*i;
-    ctx.fillText(this.text[len-i-1], y, leftX+this.width);
+  var count = this.text.length;
+  var spacing = (bottomY - topY) / (count - 1);
+  for (var i = 0; i < count; ++i) {
+    var y = bottomY - spacing*i;
+    ctx.fillText(this.text[i], y, leftX+this._width-this.settings.rightMargin);
   }
 };
