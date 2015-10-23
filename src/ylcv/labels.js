e2cbec315ba5a411214e96fe52b133b72c474918
Alex Nichol <unixpickle@gmail.com>
2015-10-22 20:25:25 -0400

    drafted implementation for DurationLabelFormat

--- src/ylcv/labels.js
+++ src/ylcv/labels.js
@@ -63,38 +63,96 @@ Labels.prototype.draw = function(ctx, leftX, topY, bottomY) {
 };
 
 var DEFAULT_MIN_SPACING = 20;
+var DEFAULT_TOP_MARGIN = 10;
 
 // DurationLabelFormat creates Labels for duration values like "1:50.50".
 function DurationLabelFormat(attrs) {
-  this.minDivision = attrs.minDivision || DurationLabelFormat.DEFAULT_MIN_DIVISION;
+  this.divisions = attrs.divisions || DurationLabelFormat.DEFAULT_DIVISIONS;
   this.decimalPlaces = attrs.decimalPlaces || DurationLabelFormat.DEFAULT_DECIMAL_PLACES;
   this.minSpacing = attrs.minSpacing || DEFAULT_MIN_SPACING;
+  this.topMargin = attrs.topMargin || DEFAULT_TOP_MARGIN;
 }
 
-DurationLabelFormat.DEFAULT_MIN_DIVISION = 250;
+DurationLabelFormat.DEFAULT_DIVISIONS = [250, 1000, 5000, 10000, 20000, 30000, 60000];
 DurationLabelFormat.DEFAULT_DECIMAL_PLACES = 2;
 
-DurationLabelFormat.prototype.createLabels = function(minValue, maxValue, height, settings) {
+DurationLabelFormat.prototype.createLabels = function(maxValue, height, settings) {
   var count = Math.floor(height / this.minSpacing);
+  var maxValueForMargin = maxValue * height / (height - this.topMargin);
+  var minDivision = maxValueForMargin / count;
 
-  // TODO: get a good division size using this.minDivision as a guide.
-  // TODO: figure out a rounded minValue and maxValue using the chosen division.
-  throw new Error('not yet implemented');
-};
+  var division = -1;
+  var error = Infinity;
+  for (var i = 0, len = this.divisions.length; i < len; ++i) {
+    // Avoid using divisions when larger divisions still give good granularity.
+    if (i < len-1 && minDivision < this.divisions[i+1]) {
+      continue;
+    }
 
-// IntegerLabelFormat creates Labels for integer values.
-function IntegerLabelFormat(attrs) {
-  this.minSpacing = attrs.minSpacing || DEFAULT_MIN_SPACING;
-}
-
-IntegerLabelFormat.prototype.createLabels = function(minValue, maxValue, height, settings) {
-  var count = Math.floor(height / this.minSpacing);
-  var division = Math.max(1, Math.floor((maxValue - minValue) / count));
+    var divisionFactor = this.divisions[i];
+    var thisDivision = divisionFactor * Math.ceil(minDivision / divisionFactor);
+    var thisError = thisDivision*count - maxValueForMargin;
+    if (thisError < error) {
+      division = thisDivision;
+      error = thisError;
+    }
+  }
 
   var values = [];
   var text = [];
   for (var i = 0; i < count; ++i) {
-    var labelValue = minValue + i*division;
+    var labelValue = i*division;
+    values[i] = labelValue;
+    if (labelValue === 0) {
+      text[i] = ''
+    } else {
+      if (labelValue % 1000 === 0) {
+        text[i] = formatDuration(labelValue, 0);
+      } else {
+        text[i] = formatDuration(labelValue, this.decimalPlaces);
+      }
+    }
+  }
+
+  return new Labels(text, values, settings);
+};
+
+function formatDuration(millis, decimalPlaces) {
+  var pow = Math.pow(10, decimalPlaces);
+  var fractional = ((Math.floor(millis / 1000 * pow) % pow) / pow).toFixed(decimalPlaces).substr(1);
+  var seconds = '' + (Math.floor(millis/1000)%60);
+  var minutes = '' + (Math.floor(millis/60000)%60);
+  var hours = '' + (Math.floor(millis/3600000)%60);
+  if (minutes === '0' && hours === '0') {
+    return seconds + fractional;
+  } else if (hours === '0') {
+    return minutes + ':' + padZero(seconds) + fractional;
+  } else {
+    seconds = padZero(seconds);
+    minutes = padZero(minutes);
+    return hours + ':' + padZero(minutes) + ':' + padZero(seconds) + fractional;
+  }
+}
+
+function padZero(s) {
+  return s.length < 2 ? '0' + s : s;
+}
+
+// IntegerLabelFormat creates Labels for integer values.
+function IntegerLabelFormat(attrs) {
+  this.minSpacing = attrs.minSpacing || DEFAULT_MIN_SPACING;
+  this.topMargin = attrs.topMargin || DEFAULT_TOP_MARGIN;
+}
+
+IntegerLabelFormat.prototype.createLabels = function(maxValue, height, settings) {
+  var count = Math.floor(height / this.minSpacing);
+  var maxValueForMargin = maxValue * height / (height - this.topMargin);
+  var division = Math.max(1, Math.ceil(maxValueForMargin / count));
+
+  var values = [];
+  var text = [];
+  for (var i = 0; i < count; ++i) {
+    var labelValue = i*division;
     values[i] = labelValue;
     if (labelValue === 0) {
       text[i] = ''
