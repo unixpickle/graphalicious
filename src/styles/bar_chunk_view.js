b363c868651ddf21f2e10b1f1dcf3e5be9171cd4
Alex Nichol <unixpickle@gmail.com>
2015-11-21 13:28:27 -0500

    BarChunkView: drafted _drawRange()
    This routine draws a range of bars within a ChunkView. It computes XMarkers in the process. It is encapsulated by draw(), which generates a DrawReport using the result from _drawRange(). This does not handle stretched drawing yet; I plan to manipulate the canvas and _drawRange() for that.

--- src/styles/bar_chunk_view.js
+++ src/styles/bar_chunk_view.js
@@ -16,7 +16,6 @@ function BarChunkView(attrs, chunk, dataSource) {
   this._animationStartTime = -1;
   this._animationType = BarChunkView.ANIMATION_NONE;
   this._animationDataPoint = null;
-  this._animationOriginalPoint = null;
   this._animationPointIndex = 0;
   this._animationInitialStartIndex = 0;
   this._animationInitialCount = 0;
@@ -30,16 +29,16 @@ BarChunkView.ANIMATION_MODIFY = 3;
 BarChunkView.ANIMATION_DURATION = 400;
 
 BarChunkView.prototype.getWidth = function() {
-  var width = this._attrs.computeRegion({startIndex: 0, length: this._dataPoints.length},
-    this._dataPoints.length).width;
-  var oldWidth = this._attrs.computeRegion({startIndex: 0, length: this._animationInitialCount},
-    this._animationInitialCount).width;
-  return this._animationProgress*width + (1-this._animationProgress)*oldWidth;
+  var landscape = this._morphingLandscape();
+  var range = {
+    startIndex: this._startIndex,
+    length: this._morphingPointCount
+  };
+  return landscape.computeRegion(range).width;
 };
 
 BarChunkView.prototype.getOffset = function() {
-  var region = this._morphingLandscape().computeRegion({startIndex: this._startIndex, 1});
-  return region.left;
+  return this._morphingLandscape().computeRegion({startIndex: this._startIndex, length: 1}).left;
 };
 
 BarChunkView.prototype.getEncompassingWidth = function() {
@@ -70,16 +69,14 @@ BarChunkView.prototype.deletion = function(oldIndex, animate) {
 BarChunkView.prototype.insertion = function(index, animate) {
   ++this._encompassingCount;
 
-  var point = this._chunk.getDataPoint(index);
-
   this.finishAnimation();
-  if (animate && this._attrs.getAnimateInsertions() &&
-      this._chunk.getLength() !== this._dataPoints.length &&
-      this._dataPoints.length !== 0) {
-    this._startAnimation(index, BarChunkView.ANIMATION_INSERT, point);
-  }
-
   if (this._chunk.getLength() > this._dataPoints.length) {
+    var point = this._chunk.getDataPoint(index - this._startIndex);
+
+    if (animate && this._attrs.getAnimateInsertions() && this._dataPoints.length !== 0) {
+      this._startAnimation(index, BarChunkView.ANIMATION_INSERT, point);
+    }
+
     this._dataPoints.splice(index-this._startIndex, 0, point);
   } else if (index < this._startIndex) {
     ++this._startIndex;
@@ -89,17 +86,19 @@ BarChunkView.prototype.insertion = function(index, animate) {
 };
 
 BarChunkView.prototype.modification = function(index, animate) {
+  assert(this._encompassingCount > 0);
+
   if (index < this._startIndex || index >= this._startIndex+this._dataPoints.length) {
     return false;
   }
 
-  var point = this._chunk.getDataPoint(index);
   this.finishAnimation();
   if (animate && this._attrs.getAnimateModifications()) {
-    this._startAnimation(index, BarChunkView.ANIMATION_MODIFY, point);
+    this._startAnimation(index, BarChunkView.ANIMATION_MODIFY,
+      this._dataPoints[index - this._startIndex]);
   }
 
-  this._animationOriginalPoint = this._dataPoints[index - this._startIndex];
+  var point = this._chunk.getDataPoint(index - this._startIndex);
   this._dataPoints[index - this._startIndex] = point;
 
   return this._animationType !== BarChunkView.ANIMATION_NONE;
@@ -111,6 +110,172 @@ BarChunkView.prototype.finishAnimation = function() {
     this._animationFrame = null;
     this._animationType = BarChunkView.ANIMATION_NONE;
   }
+};
+
+BarChunkView.prototype.draw = function(viewport, scrollX, maxValue) {
+  var totalCount = this._morphingEncompassingCount();
+  var pointCount = this._morphingPointCount();
+
+  var landscape = this._morphingLandscape();
+  var range = landscape.computeRange({left: scrollX, width: viewport.width});
+
+  assert(range.length <= totalCount && range.startIndex < totalCount);
+  assert(range.length > 0 || range.startIndex === 0);
+
+  if (range.length === totalCount) {
+    assert(range.startIndex === 0);
+    return this._drawStretched(viewport, maxValue);
+  } else if (this._startIndex + pointCount <= range.startIndex ||
+             range.startIndex + range.length <= this._startIndex) {
+    return {left: viewport.x, width: 0, xmarkers: []};
+  }
+
+  if (range.startIndex < this._startIndex) {
+    range.length -= this._startIndex - range.startIndex;
+    range.startIndex = this._startIndex;
+  }
+  if (range.startIndex + range.length > this._startIndex + pointCount) {
+    range.length = this._startIndex + pointCount - range.startIndex;
+  }
+  assert(range.length > 0);
+
+  var drawOffset = viewport.x - scrollX;
+  var xmarkers = this._drawRange(drawOffset, landscape, range, viewport, maxValue);
+
+  var region = landscape.computeRegion(range);
+  region.left += drawOffset;
+  if (region.left < viewport.x) {
+    region.width -= viewport.x - region.left;
+    region.left = viewport.x;
+  }
+  if (region.left + region.width > viewport.x + viewport.width) {
+    region.width = viewport.x + viewport.width - region.left;
+  }
+  region.xmarkers = xmarkers;
+
+  return region;
+};
+
+BarChunkView.prototype._drawStretched = function(viewport, maxValue) {
+  // TODO: this.
+};
+
+BarChunkView.prototype._drawRange = function(drawOffset, landscape, range, viewport, maxValue) {
+  var pointCount = this._morphingPointCount();
+
+  var colorScheme = this._attrs.getColorScheme();
+  var colors = [colorScheme.getPrimary(), colorScheme.getSecondary()];
+  var ctx = viewport.context;
+  var xmarkers = [];
+
+  for (var i = range.startIndex, end = range.startIndex+range.length; i < end; ++i) {
+    assert(i >= this._startIndex && i < this._startIndex + pointCount);
+
+    var dataPoint = this._morphingGetPoint(i - this._startIndex);
+    var values = [dataPoint.primary];
+    if (dataPoint.secondary) {
+      values.push(dataPoint.secondary);
+    }
+
+    var coords = landscape.computeBarRegion(i);
+    coords.left += drawOffset;
+
+    xmarkers.push(this._computeXMarker(landscape, drawOffset, i));
+
+    for (var j = 0, len = values.length; j < len; ++j) {
+      var val = values[j];
+      var height = viewport.height * (val / maxValue);
+      var y = viewport.y + viewport.height - height;
+      ctx.fillColor = colors[j];
+      ctx.fillRect(coords.left, y, coords.width, height);
+    }
+  }
+
+  return xmarkers;
+};
+
+// _computeXMarker generates an XMarker object for a bar being drawn by _drawRange.
+BarChunkView.prototype._computeXMarker = function(landscape, drawOffset, idx) {
+  assert(idx >= this._startIndex && i < this._startIndex + this._morphingPointCount());
+
+  var result = this._computeXMarkerData(idx);
+
+  var barCoords = landscape.computeBarRegion(idx);
+  var barRegion = landscape.computeRegion({startIndex: idx, length: 1});
+  switch (this._attrs.getXLabelAlignment()) {
+  case BarStyleAttrs.X_LABELS_CENTER:
+    result.x = barCoords.left + barCoords.width/2;
+    break;
+  case BarStyleAttrs.X_LABELS_RIGHT:
+    result.x = (barCoords.left + barCoords.width + barRegion.left + barRegion.width) / 2;
+    break;
+  case BarStyleAttrs.X_LABELS_LEFT:
+    result.x = (barCoords.left + barRegion.left) / 2;
+    break;
+  default:
+    throw new Error('unknown x-label alignment': this._attrs.getXLabelAlignment());
+    break;
+  }
+  result.x += drawOffset;
+
+  return result;
+};
+
+// _computeXMarkerData generates an XMarker object without the x field.
+// The supplied index should be absolute, not chunk-relative.
+BarChunkView.prototype._computeXMarkerData = function(idx) {
+  var result = {
+    visibility: 1,
+    index: idx,
+    oldIndex: idx
+  };
+
+  switch (this._animationType) {
+  case BarChunkView.ANIMATION_DELETE:
+    if (idx < this._animationPointIndex) {
+      result.dataPoint = this._dataPoints[idx - this._startIndex];
+      result.oldDataPoint = result.dataPoint;
+    } else if (idx === this._animationPointIndex) {
+      result.index = -1;
+      result.oldIndex = this._animationPointIndex;
+      result.dataPoint = null;
+      result.oldDataPoint = this._animationDataPoint;
+      result.visibility = 1 - this._animationProgress;
+    } else {
+      --result.index;
+      result.dataPoint = this._dataPoints[idx - 1 - this._startIndex];
+      result.oldDataPoint = result.dataPoint;
+    }
+    break;
+  case BarChunkView.ANIMATION_MODIFY:
+    var newPoint = this._dataPoints[idx - this._startIndex];
+    result.dataPoint = newPoint;
+    if (idx !== this._animationPointIndex) {
+      result.oldDataPoint = newPoint;
+    } else {
+      result.oldDataPoint = this._animationDataPoint;
+    }
+    break;
+  case BarChunkView.ANIMATION_INSERT:
+    result.dataPoint = this._dataPoints[idx - this._startIndex];
+    if (idx === this._animationPointIndex) {
+      result.oldDataPoint = null;
+      result.oldIndex = -1;
+      result.visibility = this._animationProgress;
+    } else {
+      result.oldDataPoint = result.dataPoint;
+    }
+    if (idx > this._animationPointIndex) {
+      --result.oldIndex;
+    }
+    break;
+  case BarChunkView.ANIMATION_NONE:
+    result.dataPoint = this._dataPoints[idx - this._startIndex];
+    result.oldDataPoint = result.dataPoint;
+    break;
+  }
+
+  return result;
 };
 
 BarChunkView.prototype._animate = function(time) {
@@ -140,7 +305,6 @@ BarChunkView.prototype._startAnimation = function(index, type, point) {
   this._animationInitialCount = this._dataPoints.length;
   this._animationPointIndex = index;
   this._animationDataPoint = point;
-  this._animaitonOriginalPoint = null;
   this._animationProgress = 0;
   this._animationStartTime = -1;
   this._animationFrame = window.requestAnimationFrame(this._animate.bind(this));
@@ -150,21 +314,78 @@ BarChunkView.prototype._startAnimation = function(index, type, point) {
 BarChunkView.prototype._morphingLandscape = function() {
   var properties = {
     attrs: this._attrs,
-    pointCount: this._pointCount,
-    morphingIndex: this._animationPointIndex,
-    morphingVisibility: this._animationProgress
+    pointCount: this._morphingEncompassingCount(),
+    morphingIndex: 0,
+    morphingVisibility: 1
   };
 
-  switch (this._animationType) {
-  case BarChunkView.ANIMATION_DELETE:
-    properites.pointCount += 1;
-  case BarChunkView.ANIMATION_INSERT:
-    break;
-  default:
-    properties.morphingVisibility = 1;
-    properties.morphingIndex = 0;
-    break;
+  if (this._animationType === BarChunkView.ANIMATION_DELETE ||
+      this._animationType === BarChunkView.ANIMATION_INSERT) {
+    properties.morphingIndex = this._animationPointIndex;
+    properties.morphingVisibility = this._animationProgress;
   }
 
   return new MorphingBarLandscape(properties);
+};
+
+// _morphingPointCount returns the number of points in this ChunkView's partial morphing landscape.
+BarChunkView.prototype._morphingPointCount = function() {
+  if (this._animationType === BarChunkView.ANIMATION_DELETE) {
+    return this._pointCount + 1;
+  } else {
+    return this._pointCount;
+  }
+};
+
+// _morphingEncompassingCount returns the number of data points in the complete morphing landscape.
+BarChunkView.prototype._morphingEncompassingCount = function() {
+  if (this._animationType === BarChunkView.ANIMATION_DELETE) {
+    return this._encompassingCount + 1;
+  } else {
+    return this._encompassingCount;
+  };
+};
+
+// _morphingGetPoint returns the data point at a chunk-relative index in the partial morphing
+// landscape.
+// This will handle deleted points and modified points in a special way.
+BarChunkView.prototype._morphingGetPoint = function(idx) {
+  assert(idx >= 0);
+  if (this._animationType === BarChunkView.ANIMATION_DELETE) {
+    assert(idx < this._pointCount + 1);
+    if (idx + this._startIndex < this._animationPointIndex) {
+      return this._dataPoints[idx];
+    } else if (idx + this._startIndex === this._animationPointIndex) {
+      return this._animationDataPoint;
+    } else {
+      return this._dataPoints[idx - 1];
+    }
+  } else if (this._animationType === BarChunkView.ANIMATION_MODIFY) {
+    assert(idx < this._pointCount);
+
+    var newPoint = this._dataPoints[idx];
+    if (idx + this._startIndex !== this._animationPointIndex) {
+      return newPoint;
+    }
+    var oldPoint = this._animationDataPoint;
+
+    var p = this._animationProgress;
+    var np = 1 - this._animationProgress;
+
+    var intermediatePoint = {
+      primary: p*newPoint.primary + np*oldPoint.primary,
+      secondary: -1
+    };
+
+    if (newPoint.secondary < 0 && oldPoint.secondary >= 0) {
+      intermediatePoint.secondary = np * oldPoint.secondary;
+    } else if (newPoint.secondary >= 0 && oldPoint.secondary < 0) {
+      intermediatePoint.secondary = p * newPoint.secondary;
+    }
+
+    return intermediatePoint;
+  } else {
+    assert(idx < this._pointCount);
+    return this._dataPoints[idx];
+  }
 };
