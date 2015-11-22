dcadf9cf1af161ac1557ec804faa1fc8c72093db
Alex Nichol <unixpickle@gmail.com>
2015-11-22 14:23:14 -0500

    BarChunkView: basic scrolling works

--- test/bar_chunk_view_test.js
+++ test/bar_chunk_view_test.js
@@ -75,6 +75,80 @@ function testDrawBestCase() {
   }
 }
 
+function testDrawBasicScrolling() {
+  var xLabelOffsets = [
+    [BarStyle.X_LABELS_LEFT, 5, 52.5],
+    [BarStyle.X_LABELS_CENTER, 30, 75],
+    [BarStyle.X_LABELS_RIGHT, 52.5, 97.5]
+  ];
+
+  for (var j = 0; j < 3; ++j) {
+    var xInfo = xLabelOffsets[j];
+
+    var style = new BarStyle({
+      colorScheme: new ColorScheme('#65bcd4', '#325e6a'),
+      leftMargin: 10,
+      rightMargin: 10,
+      barSpacing: 5,
+      barWidth: 40,
+      xLabelAlignment: xInfo[0]
+    });
+
+    var dataSource = DataSource.random(1000, 10, true);
+    dataSource.insert(3, {primary: 5, secondary: -1});
+    var chunk = dataSource.fetchChunkSync(0, 0, 100);
+
+    var chunkView = style.createChunkView(chunk, dataSource);
+    var context = new Canvas().getContext('2d');
+
+    var viewport = {context: context, x: 10, y: 5, width: 313, height: 100};
+
+    var report = chunkView.draw(viewport, 3, 20);
+    assert(Math.abs(report.width - 313) < SMALL_NUM, 'invalid width');
+    assert(Math.abs(report.left - 10) < SMALL_NUM, 'invalid left offset');
+
+    assert(report.xmarkers.length === 7);
+    for (var i = 0, len = report.xmarkers.length; i < len; ++i) {
+      var marker = report.xmarkers[i];
+      assert(marker.index === i);
+      assert(marker.oldIndex === i);
+      assert(marker.dataPoint === chunk.getDataPoint(i));
+      assert(marker.oldDataPoint === chunk.getDataPoint(i));
+      assert(marker.visibility === 1);
+    }
+
+    var firstXValue = xInfo[1] + viewport.x - 3;
+    assert(Math.abs(report.xmarkers[0].x - firstXValue) < SMALL_NUM, 'invalid x for xmarkers[0]');
+
+    for (var i = 1, len = report.xmarkers.length; i < len; ++i) {
+      var marker = report.xmarkers[i];
+      var xValue = viewport.x + xInfo[2] + (i-1)*45 - 3;
+      assert(Math.abs(marker.x - xValue) < SMALL_NUM, 'invalid x for xmarkers[' + i + ']');
+    }
+
+    var report = chunkView.draw(viewport, 51, 20);
+    assert(Math.abs(report.width - 313) < SMALL_NUM, 'invalid width');
+    assert(Math.abs(report.left - 10) < SMALL_NUM, 'invalid left offset');
+
+    assert(report.xmarkers.length === 7);
+    for (var i = 0, len = report.xmarkers.length; i < len; ++i) {
+      var marker = report.xmarkers[i];
+      assert(marker.index === i+1);
+      assert(marker.oldIndex === i+1);
+      assert(marker.dataPoint === chunk.getDataPoint(i+1));
+      assert(marker.oldDataPoint === chunk.getDataPoint(i+1));
+      assert(marker.visibility === 1);
+    }
+
+    for (var i = 0, len = report.xmarkers.length; i < len; ++i) {
+      var marker = report.xmarkers[i];
+      var xValue = viewport.x + xInfo[2] + i*45 - 51;
+      assert(Math.abs(marker.x - xValue) < SMALL_NUM, 'invalid x for xmarkers[' + i + ']');
+    }
+  }
+}
+
 testDrawBestCase();
+testDrawBasicScrolling();
 
 console.log('PASS');
