8a6ebf27f81b9bdb1f18d78eae54a9d35dc6a725
Alex Nichol <unixpickle@gmail.com>
2015-12-09 19:56:59 -0500

    copy attributes for CurveChunkView

--- src/styles/curve_style.js
+++ src/styles/curve_style.js
@@ -7,7 +7,8 @@ function CurveStyle(attrs) {
 CurveStyle.prototype = Object.create(DotStyle.prototype);
 
 CurveStyle.prototype.createChunkView = function(chunk, dataSource) {
-  return new CurveChunkView(this._dotAttrs, this.copyAttributes(), chunk, dataSource);
+  return new CurveChunkView(this._dotAttrs.copyAttributes(), this.copyAttributes(), chunk,
+    dataSource);
 };
 
 exports.CurveStyle = CurveStyle;
