24cbe9d229ab342d91209fa36138570b3710903b
Alex Nichol <unixpickle@gmail.com>
2015-12-11 19:26:27 -0500

    drafted FullCurve measurement system

--- src/styles/full_curve_chunk_view.js
+++ src/styles/full_curve_chunk_view.js
@@ -2,10 +2,100 @@
 
 function FullCurveChunkView(attrs, chunk, dataSource) {
   EventEmitter.call(this);
-  
-  // TODO: save more information here.
+
+  this._attrs = attrs;
+  this._chunk = chunk;
+  this._dataSource = dataSource;
 }
 
 FullCurveChunkView.prototype = Object.create(EventEmitter.prototype);
 
-// TODO: implement drawing, etc.
+FullCurveChunkView.prototype.getWidth = function() {
+  return this._attrs.computeRegion({
+    startIndex: this._chunk.getStartIndex(),
+    length: this._chunk.getLength()
+  }, this._dataSource.getLength()).width;
+};
+
+FullCurveChunkView.prototype.getOffset = function() {
+  return this._attrs.computeRegion({
+    startIndex: this._chunk.getStartIndex(),
+    length: this._chunk.getLength()
+  }, this._dataSource.getLength()).left;
+};
+
+FullCurveChunkView.prototype.getEncompassingWidth = function() {
+  return this._attrs.computeRegion({
+    startIndex: 0,
+    length: this._dataSource.getLength()
+  }, this._dataSource.getLength()).width;
+};
+
+FullCurveChunkView.prototype.finishAnimation = function() {
+};
+
+FullCurveChunkView.prototype.deletion = function(oldIndex, animate) {
+  return false;
+};
+
+FullCurveChunkView.prototype.insertion = function(index, animate) {
+  return false;
+};
+
+FullCurveChunkView.prototype.modification = function(index, animate) {
+  return false;
+}
+
+FullCurveChunkView.prototype.draw = function(viewport, scrollX, maxValue) {
+  if (this._dataSource.getLength() === 0) {
+    return {
+      left: viewport.x,
+      width: 0,
+      xmarkers: []
+    };
+  }
+
+  viewport.context.lineWidth = this._attrs.getLineThickness();
+  viewport.context.strokeStyle = this._attrs.getColorScheme().getPrimary();
+
+  // TODO: do something like this:
+  //
+  // if (pointSpacing > FullCurveChunkView.MIN_SPACING_FOR_SMOOTH) {
+  //   this._drawSmooth(startX, pointSpacing, viewport, maxValue);
+  // } else {
+  //   this._drawStraight(startX, pointSpacing, viewport, maxValue);
+  // }
+
+  throw new Error('not yet implemented');
+};
+
+FullCurveChunkView.prototype._drawSmooth = function(startX, spacing, viewport, max) {
+  var connectPoints = [];
+  for (var i = 0, len = this._chunk.getLength(); i < len; ++i) {
+    var dataPoint = this._chunk.getDataPoint(i);
+    var x = startX + i*spacing;
+
+    var primaryRatio = dataPoint.primary / max;
+    var y = viewport.y + (1-primaryRatio)*viewport.height;
+
+    connectPoints.push({x: x, y: y});
+  }
+
+  var smoothPoints = smoothPath(connectPoints, 1);
+  var ctx = viewport.context;
+  ctx.beginPath();
+  for (var i = 0, len = smoothPoints.length; i < len; ++i) {
+    var point = smoothPoints[i];
+    if (i === 0) {
+      ctx.moveTo(point.x, point.y);
+    } else {
+      ctx.lineTo(point.x, point.y);
+    }
+  }
+  ctx.strokePath();
+  ctx.closePath();
+};
+
+FullCurveChunkView.prototype._drawStraight = function(startX, spacing, viewport, max) {
+
+};
--- src/styles/full_curve_style.js
+++ src/styles/full_curve_style.js
@@ -5,15 +5,80 @@ function FullCurveStyleAttrs(attrs) {
     FullCurveStyleAttrs.DEFAULTS);
 }
 
-FullCurveStyleAttrs.ATTRIBUTES = ['colorScheme', 'leftMargin', 'rightMargin', 'lineThickness'];
+FullCurveStyleAttrs.ATTRIBUTES = ['colorScheme', 'leftMargin', 'rightMargin', 'lineThickness',
+  'minWidth'];
 
 FullCurveStyleAttrs.DEFAULTS = {
   leftMargin: 0,
   rightMargin: 0,
-  lineThickness: 5
+  lineThickness: 5,
+  minWidth: 0
 };
 
 defineAttributeMethods(FullCurveStyleAttrs, FullCurveStyleAttrs.ATTRIBUTES);
+
+FullCurveStyleAttrs.prototype.totalMargins = function() {
+  return this.getLeftMargin() + this.getRightMargin();
+};
+
+FullCurveStyleAttrs.prototype.realMinWidth = function() {
+  return Math.max(this.getMinWidth(), this.totalMargins());
+};
+
+FullCurveStyleAttrs.prototype.computeRange = function(region, pointCount) {
+  var totalWidth = this.computeRegion({startIndex: 0, length: pointCount}, pointCount);
+  region = regionIntersection(region, {left: 0, width: totalWidth});
+
+  if (pointCoint === 0) {
+    return {startIndex: 0, length: 0};
+  } else if (region.width === 0) {
+    return {startIndex: 0, length: 0};
+  } else if (pointCount === 1) {
+    return {startIndex: 0, length: 1};
+  }
+
+  var availableWidth = this.realMinWidth() - this.totalMargins();
+  var pointSpacing = availableWidth / (pointCount - 1);
+
+  var startIndex = Math.floor((region.left - this.getLeftMargin()) / pointSpacing);
+  var endIndex = Math.ceil((region.left + region.width - this.getLeftMargin) / pointSpacing);
+
+  var unboundedRange = {
+    startIndex: startIndex,
+    endIndex: endIndex - startIndex
+  };
+
+  return rangeIntersection(unboundedRange, {startIndex: 0, length: pointCount});
+};
+
+FullCurveStyleAttrs.prototype.computeRegion = function(range, pointCount) {
+  range = rangeIntersection(range, {startIndex: 0, length: pointCount});
+
+  if (range.length === 0) {
+    return 0;
+  } else if (pointCount === 1) {
+    return this.realMinWidth();
+  }
+
+  assert(pointCount > 1);
+
+  var availableWidth = this.realMinWidth() - this.totalMargins();
+  var pointSpacing = availableWidth / (pointCount - 1);
+
+  var startLeft = 0;
+  if (range.startIndex > 0) {
+    startLeft = this._attrs.getLeftMargin() + pointSpacing*this._chunk.getStartIndex();
+  }
+
+  var endLeft = this._attrs.realMinWidth();
+  var chunkEndIndex = range.startIndex + range.length;
+  if (chunkEndIndex < pointCount) {
+    var countAfterEnd = pointCount - chunkEndIndex;
+    endLeft -= this.getRightMargin() + pointSpacing*countAfterEnd;
+  }
+
+  return {left: startLeft, width: endLeft-startLeft};
+};
 
 function FullCurveStyle(attrs) {
   EventEmitter.call(this);
@@ -30,11 +95,11 @@ FullCurveStyle.prototype.setAttributes = function(attrs) {
 };
 
 FullCurveStyle.prototype.computeRange = function(region, pointCount) {
-  return {startIndex: 0, length: pointCount};
+  return this._attrs.computeRange(region, pointCount);
 };
 
 FullCurveStyle.prototype.computeRegion = function(range, pointCount) {
-  return {left: 0, width: 0};
+  return this._attrs.computeRange(range, pointCount);
 };
 
 FullCurveStyle.prototype.createChunkView = function(chunk, dataSource) {
