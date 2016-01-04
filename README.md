7ca41e1e696d7a93156a8221fb05a89a2076c5d1
Alex Nichol <unixpickle@gmail.com>
2016-01-04 13:38:27 -0500

    implemented blurbs for dot/curve chunk view

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ You must have [jsbuild](https://github.com/unixpickle/jsbuild) installed in orde
 
  * Implement blurbs
    * Test blurbs with stretching
-   * Implement blurbs for dot graphs
+   * Fix issue where arrow of upward facing blurb goes under scrollbar
  * Figure out how alternating x-axis labels will work
  * Implement YLCV subclass for x-axis labels
 
--- src/styles/dot_chunk_view.js
+++ src/styles/dot_chunk_view.js
@@ -5,6 +5,10 @@ function DotChunkView(dotAttrs, attrs, chunk, dataSource) {
   this._dotAttrs = dotAttrs;
 }
 
+// DotChunkView.HOVER_NEAR_RADIUS is the maximum distance a user can be from a dot before
+// it registers as a hover event.
+DotChunkView.HOVER_NEAR_RADIUS = 20;
+
 DotChunkView.prototype = Object.create(BarChunkView.prototype);
 DotChunkView.prototype.constructor = DotChunkView;
 
@@ -78,4 +82,69 @@ DotChunkView.prototype._drawCircle = function(params, x, y, radius) {
   ctx.fill();
   ctx.closePath();
   ctx.fillStyle = oldFill;
+};
+
+DotChunkView.prototype._computeHoverInformation = function(pointerPos, viewport, maxValue) {
+  var landscape = this._morphingLandscape();
+  var maxRadius = DotChunkView.HOVER_NEAR_RADIUS + this._attrs.getBarWidth()/2;
+  var range = landscape.computeRange({left: pointerPos.x-maxRadius, width: maxRadius*2});
+
+  var closestDistance = maxRadius;
+  var closestResult = null;
+
+  for (var i = range.startIndex-1; i <= range.startIndex+range.length; ++i) {
+    var points = this._hoverInfosForPointIndex(i, viewport, maxValue, landscape);
+    for (var j = 0, len = points.length; j < len; ++j) {
+      var point = points[j];
+      var dist = Math.sqrt(Math.pow(point.position.x-pointerPos.x, 2) +
+        Math.pow(point.position.y-pointerPos.y, 2));
+      if (dist <= closestDistance) {
+        closestDistance = dist;
+        closestResult = point;
+      }
+    }
+  }
+
+  return closestResult;
+};
+
+DotChunkView.prototype._hoverInfosForPointIndex = function(index, viewport, maxValue, landscape) {
+  if (index < this._startIndex || index >= this._startIndex+this._morphingPointCount()) {
+    return [];
+  }
+
+  var point = this._morphingGetPoint(index - this._startIndex);
+  var region = landscape.computeBarRegion(index);
+  var radius = this._attrs.getBarWidth() / 2;
+
+  var res = [];
+  for (var i = 0; i < 2; ++i) {
+    if ((i === 0 && !point.hasOwnProperty('primaryTooltip')) ||
+        (i === 1 && (point.secondary < 0 || !point.hasOwnProperty('secondaryTooltip')))) {
+      continue;
+    }
+
+    var value = (i === 0 ? point.primary : point.secondary);
+    var text = (i === 0 ? point.primaryTooltip : point.secondaryTooltip);
+
+    var height = (value / maxValue) * viewport.height;
+    var y = viewport.y + viewport.height - height;
+    if (this._dotAttrs.getBottomMargin() > height) {
+      y += height - this._dotAttrs.getBottomMargin();
+    }
+
+    var centerX = region.left + region.width/2;
+    if (index === 0) {
+      centerX = region.left + radius;
+    } else if (index === this._morphingEncompassingCount()-1) {
+      centerX = region.left + region.width - radius;
+    }
+
+    res.push({
+      position: {x: centerX, y: y},
+      text: text
+    });
+  }
+
+  return res;
 };
